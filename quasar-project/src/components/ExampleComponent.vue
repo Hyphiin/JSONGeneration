9f0347869b506eb2ffc1778b265c61587aee7be5
Chiara <chiara.knipprath@iem.thm.de>
2023-05-22 11:46:50 +0200

    first commit

---
+++ quasar-project/src/components/ExampleComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>Moin!</h2>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'ExampleComponent',
+  props: {},
+  setup() {
+    return {};
+  },
+});
+</script>
